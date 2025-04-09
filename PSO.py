c56647180563f0a36e9abf12d7b2f41f8b136fa8
Raahim Hashmi <116343264+RaahimHash@users.noreply.github.com>
2025-04-09 14:46:32 +0500

    Added notebooks and final files

--- PSO.py
+++ PSO.py
@@ -2,29 +2,34 @@ import math
 import random
 import matplotlib.pyplot as plt
 
-def rosenbrock(x, y):
+def rosenbrock(pos):
+    x, y = pos[0], pos[1]
     if -2 < x < 2 and -1 < y < 3:
         return 100*(x**2 - y)**2 + (1 - x)**2
     else:
         print(f"Rosenbrock function is only defined for x in (-2, 2) and y in (-1, 3). Given x: {x}, y: {y}")
-        return float('inf') 
+        return float('inf') # really bad fitness if invalid input
 
-def greiwank(x, y):
+def greiwank(pos):
+    x, y = pos[0], pos[1]
     if -30 < x < 30 and -30 < y < 30:
         return 1+ (x**2 + y**2)/4000 - math.cos(x)*math.cos(y/math.sqrt(2))
     else:
         print(f"Greiwank function is only defined for x and y in (-30, 30). Given x: {x}, y: {y}")
-        return float('inf')
+        return float('inf') 
+
+ROS = 0
+GREI = 1
 
 def init_population(pop_size, bounds):
 
-    dim = len(bounds)
+    dim = len(bounds) # the number of bounds is the dimension of the input, e.g. for a 3D function, there would be 2 bounds on the input
     positions = []
     velocities = []
 
     for _ in range(pop_size):
-        position = [random.uniform(bounds[d][0], bounds[d][1]) for d in range(dim)]
-        velocity = [random.uniform(-5, 5) for _ in range(dim)]
+        position = [random.uniform(bounds[d][0], bounds[d][1]) for d in range(dim)] # uniformly generate a position in the bounds
+        velocity = [random.uniform(-1, 1) for _ in range(dim)] # between -1 and 1 so there is no velocity such that it always goes out of bounds no matter the position, doesn't make much difference
         positions.append(position)
         velocities.append(velocity)
 
@@ -33,12 +38,15 @@ def init_population(pop_size, bounds):
 def update(positions, velocities, p_best, g_best, w, c1, c2, bounds):
 
     dim = len(bounds)
+
     for i in range(len(positions)):
-        for d in range(dim):
+        for d in range(dim): 
+            
             positions[i][d] += velocities[i][d]
             
+            # since the bounds are exclusive, offset the position by a small amount to ensure it is within the bounds
             if positions[i][d] < bounds[d][0]:
-                positions[i][d] = bounds[d][0] + 0.001
+                positions[i][d] = bounds[d][0] + 0.001 
             elif positions[i][d] > bounds[d][1]:
                 positions[i][d] = bounds[d][1] - 0.001
             
@@ -46,7 +54,8 @@ def update(positions, velocities, p_best, g_best, w, c1, c2, bounds):
             r2 = random.random()
 
             velocities[i][d] = w*velocities[i][d] + c1*r1*(p_best[i][d] - positions[i][d]) + c2*r2*(g_best[d] - positions[i][d])
-    
+
+            # clamp the velocity to be between -1 and 1
             if velocities[i][d] < -1:
                 velocities[i][d] = -1
             elif velocities[i][d] > 1:
@@ -55,8 +64,8 @@ def update(positions, velocities, p_best, g_best, w, c1, c2, bounds):
 def fitness(positions, p_best, g_best, fitness_func):
     
     for i in range(len(positions)):
-        fit = fitness_func(positions[i][0], positions[i][1])
-        if fit != float('inf'):
+        fit = fitness_func(positions[i]) 
+        if fit != float('inf'): # valid fitness value
             if fit < p_best[i][2]:
                 p_best[i][0] = positions[i][0]
                 p_best[i][1] = positions[i][1]
@@ -72,30 +81,30 @@ def init_pso(pop_size, bounds, fitness_func):
     
     positions, velocities = init_population(pop_size, bounds)
     
-    p_best = [[positions[i][0], positions[i][1], fitness_func(positions[i][0], positions[i][1])] for i in range(pop_size)]
-    g_best = [positions[0][0], positions[0][1], fitness_func(positions[0][0], positions[0][1])]
+    p_best = [[positions[i][0], positions[i][1], fitness_func(positions[i])] for i in range(pop_size)]
+    g_best = [p_best[0][0], p_best[0][1], p_best[0][2]] # assume the first particle is the fittest
 
-    for i in range(pop_size):
-        if p_best[i][2] < g_best[2]:
+    for i in range(1, pop_size):
+        if p_best[i][2] < g_best[2]: # if the ith particle is more fit than the currently assumed best 
             g_best[0] = p_best[i][0]
             g_best[1] = p_best[i][1]
             g_best[2] = p_best[i][2]
 
     return positions, velocities, p_best, g_best
 
-func = 1 # 0 for Rosenbrock, 1 for Greiwank
-POPULATION_SIZE = 30
-if func == 0:
+func = GREI # ROS or GREI, the function to be minimized
+POPULATION_SIZE = 1000
+MAX_ITERATIONS = 250 
+W = 0.5 # inertia weight
+C1 = 1.5 # cognitive coefficient
+C2 = 1.5 # social coefficient
+
+if func == ROS:
     BOUNDS = [(-2, 2), (-1, 3)]  # Rosenbrock function bounds
     positions, velocities, p_best, g_best = init_pso(POPULATION_SIZE, BOUNDS, rosenbrock)
 else:
     BOUNDS = [(-30, 30), (-30, 30)]  # Greiwank function bounds
     positions, velocities, p_best, g_best = init_pso(POPULATION_SIZE, BOUNDS, greiwank)
-
-MAX_ITERATIONS = 250
-W = 0.5
-C1 = 1.5
-C2 = 1.5
 
 iters = 0
 bests = []
@@ -124,3 +133,12 @@ plt.title('Fitness vs Iterations')
 plt.legend()
 plt.grid()
 plt.show()
+
+'''
+Results:
+For Rosenbrock, (x, y) = (1, 1) is the minima with f(x,y) = 0
+For Greiwank, there are several minimas with f(x,y) = 0, some are :
+(x, y) = (2.1744217553179356e-09, -1.4689605454118375e-08)
+(x, y) = (8.917850510230875e-09, -5.8815555734523115e-09)
+(x, y) = (-6.296560183007317e-10, -5.7935971703084705e-09)
+'''
--- ValueIteration.py
+++ ValueIteration.py
@@ -1,36 +1,29 @@
 import numpy as np
 import gymnasium as gym
-import random
 import math
 
 env = gym.make("Taxi-v3", render_mode="human")  
 
-# change-able parameters:
-discount_factor = 0.9
-delta_threshold = 0.00000001
-# epsilon = 1 # ???
-
 def value_iteration(env, discount_factor, delta_threshold):
 
-    def find_max(state_rewards, V):
+    def find_max(state_rewards, V): # find the max value action from a given state
 
-        value = -math.inf
+        value = -math.inf # initialize to negative infinity
         index = 0
 
         for i in range(len(state_rewards)):
 
-            d = discount_factor
-            if V[state_rewards[i][0]] < 0: # if the value of the state is negative, it becomes MORE negatve after discounting
-                d = 2 - d
+            d = discount_factor 
+            # if V[state_rewards[i][0]] < 0: # If the value of the state is negative, it becomes MORE negatve after discounting. Results are similar with or without this.
+            #     d = 2 - d
 
             if value < (state_rewards[i][1] + d * V[state_rewards[i][0]]):
                 value = state_rewards[i][1] + d * V[state_rewards[i][0]]
                 index = i
 
-        return value, index
+        return index, value
 
     num_states = env.observation_space.n
-    # num_actions = env.action_space.n
 
     # Initialize the value function
     V = np.zeros(num_states)
@@ -38,28 +31,28 @@ def value_iteration(env, discount_factor, delta_threshold):
     # For each state, the policy will tell you the action to take
     policy = np.zeros(num_states, dtype=int)
 
-    #Write your code to implement value iteration main loop
-
-    env_unwrapped = env.unwrapped # saw this in some random discussion post online, it's necessary to use the P attribute
+    env_unwrapped = env.unwrapped # saw this in some random discussion post online, it's necessary to use the P attribute at line 48
 
     while True:
 
-        delta = 0
+        delta = 0 # Initialize delta to 0 for each iteration
 
-        for state in range(num_states):
-            actions = env_unwrapped.P[state]
-            state_rewards = [(actions[i][0][1], actions[i][0][2]) for i in range(len(actions))]
-            # print(state_rewards)
-            v = V[state]
-            V[state], policy[state] = find_max(state_rewards, V) # find max value and action index
-            delta = max(delta, abs(v - V[state]))
+        for state in range(num_states): # for each state 
 
-        # print("Delta:", delta)
-        if delta < delta_threshold:
+            actions = env_unwrapped.P[state] # get all actions
+            state_rewards = [(actions[i][0][1], actions[i][0][2]) for i in range(len(actions))] # get the states and rewards for each action from a certain state and store it as a (state, reward) tuple
+            
+            v = V[state] # store the current value of the state
+            policy[state], V[state] = find_max(state_rewards, V) # update the value and policy
+            delta = max(delta, abs(v - V[state])) # update delta to the max difference between the old and new value of the state
+
+        if delta < delta_threshold: # if there were no changes bigger than the threshold, we can stop the iteration
             break
 
     return policy, V
 
+discount_factor = 0.9
+delta_threshold = 0.00000001
 
 # Run value iteration
 policy, V = value_iteration(env, discount_factor, delta_threshold)
@@ -71,6 +64,7 @@ step = 0
 done = False
 state = state[0]
 max_steps = 100
+
 for step in range(max_steps):
 
     # Getting max value against that state, so that we choose that action
